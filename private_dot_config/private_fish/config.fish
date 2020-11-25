f2240e255dd4a1a7297c4d2d94d3ea5b803728f2
Brandon Nguyen <gnuyent@protonmail.com>
2020-11-24 17:03:13 -0800

    Chezmoi

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,63 @@
+set --export SHELL /usr/bin/fish
+set --export EDITOR nvim
+set --export DOTBARE_DIR $HOME/.config/dotfiles
+
+# Custom bin
+if test -e $HOME/.local/bin
+    set PATH $HOME/.local/bin $PATH
+end
+
+# Rust
+if test -e $HOME/.cargo/bin
+    set PATH $HOME/.cargo/bin $PATH
+end
+
+# Dotbare
+if test -e $HOME/.dotbare
+    set PATH $HOME/.dotbare $PATH
+end
+
+# Dotnet
+if test -e $HOME/.dotnet
+    set PATH $HOME/.dotnet $PATH
+end
+
+# cowsay
+function fish_greeting
+    fortune | cowsay
+end
+
+# ctrl+z
+function fish_user_key_bindings
+    bind \cz 'fg 2>/dev/null; commandline -f repaint'
+end
+
+# ls
+alias ls='exa --icons'
+alias la='exa -a --icons'
+alias ll='exa -l --icons'
+alias lal='exa -l -a --icons'
+
+# git
+alias gcl='git clone'
+alias gcm='git commit -m'
+
+# grep
+alias grep='rg'
+
+# cat
+alias cat='bat --theme=OneHalfDark -p'
+
+# vim
+alias vi='nvim'
+alias vim='nvim'
+
+# tmux
+alias tmux='tmux -f $HOME/.config/tmux/tmux.conf'
+
+# ifconfig
+alias ifconfig='ip -c addr'
+
+# nvim nightly
+alias nvim-upgrade='bash $HOME/.local/bin/nvim_build_script.sh'
+
