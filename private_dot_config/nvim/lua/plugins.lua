1511a6db8ee7700415ceeef46c8f25c5eb23915e
Brandon Nguyen <gnuyent@protonmail.com>
2021-02-07 17:38:30 -0800

    Update plugins

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -69,43 +69,95 @@ return require('packer').startup(function()
     'neovim/nvim-lspconfig',
     config = function()
       local lspconfig = require'lspconfig'
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
       -- LSP Configurations
       -- Go
-      lspconfig.gopls.setup{}
+      lspconfig.gopls.setup{
+        capabilities = capabilities
+      }
 
       -- Rust
       lspconfig.rust_analyzer.setup{
         root_dir = lspconfig.util.root_pattern('Cargo.toml', 'rust-project.json'),
-        on_attach = require'completion'.on_attach
+        capabilities = capabilities
       }
     end
   }
 
   use {
-    'nvim-lua/completion-nvim',
+    'hrsh7th/nvim-compe',
     requires = {
-      {
-        'SirVer/ultisnips',
-        config = function()
-          vim.g.UltiSnipsExpandTrigger = '<Plug>(placeholder)'
-          vim.g.UltiSnipsJumpForwardTrigger = '<tab>'
-          vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
-        end
-      },
-      {'honza/vim-snippets'}
+      {'hrsh7th/vim-vsnip'}
     },
     config = function()
-      -- Use <Tab> and <S-Tab> to navigate through popup menu
-      vim.cmd('inoremap <expr> <Tab>   pumvisible() ? "\\<C-n>" : "\\<Tab>"')
-      vim.cmd('inoremap <expr> <S-Tab> pumvisible() ? "\\<C-p>" : "\\<S-Tab>"')
-
       -- Set completeopt to have a better completion experience
-      vim.cmd('set completeopt=menuone,noinsert,noselect')
+      vim.o.completeopt = "menu,menuone,noselect"
 
-      -- Avoid showing message extra message when using completion
-      vim.cmd('set shortmess+=c')
+      require'compe'.setup {
+          enabled = true;
+          autocomplete = true;
+          debug = false;
+          min_length = 1;
+          preselect = 'enable';
+          throttle_time = 80;
+          source_timeout = 200;
+          incomplete_delay = 400;
+          max_abbr_width = 100;
+          max_kind_width = 100;
+          max_menu_width = 100;
 
-      vim.g.completion_enable_snippet = 'UltiSnips'
+          source = {
+              path = true;
+              buffer = true;
+              calc = true;
+              vsnip = true;
+              nvim_lsp = true;
+              nvim_lua = true;
+              spell = true;
+              tags = true;
+              snippets_nvim = true;
+              treesitter = true;
+          };
+      }
+
+      local t = function(str)
+          return vim.api.nvim_replace_termcodes(str, true, true, true)
+      end
+
+      -- Use (s-)tab to:
+      --- move to prev/next item in completion menuone
+      --- jump to prev/next snippet's placeholder
+      _G.tab_complete = function()
+        if vim.fn.pumvisible() == 1 then
+          return t "<C-n>"
+        elseif vim.fn.call("vsnip#available", {1}) == 1 then
+          return t "<Plug>(vsnip-expand-or-jump)"
+        else
+          return t "<Tab>"
+        end
+      end
+
+      _G.s_tab_complete = function()
+        if vim.fn.pumvisible() == 1 then
+          return t "<C-p>"
+        elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+          return t "<Plug>(vsnip-jump-prev)"
+        else
+          return t "<S-Tab>"
+        end
+      end
+
+      vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+      vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+      vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+      vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+      vim.cmd("inoremap <silent><expr> <C-Space> compe#complete()")
+      vim.cmd("inoremap <silent><expr> <CR>      compe#confirm('<CR>')")
+      vim.cmd("inoremap <silent><expr> <C-e>     compe#close('<C-e>')")
     end
   }
 
