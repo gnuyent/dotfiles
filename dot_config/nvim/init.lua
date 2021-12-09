30cfe21d299d82e4b8a2bbbf186e7f32d7659187
Brandon Nguyen <gnuyent@protonmail.com>
2021-12-09 15:50:01 -0800

    reformat init

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,11 +7,10 @@ if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
 end
 
 -- Bootstrap Hotpot
-local hotpot_path = vim.fn.stdpath('data')..'/site/pack/packer/start/hotpot.nvim'
+local hotpot_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/hotpot.nvim"
 if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
-  print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
-  vim.fn.system({'git', 'clone',
-                 'https://github.com/rktjmp/hotpot.nvim', hotpot_path})
+	print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
+	vim.fn.system({ "git", "clone", "https://github.com/rktjmp/hotpot.nvim", hotpot_path })
 end
 
 -- Bootstrap .fnl support
